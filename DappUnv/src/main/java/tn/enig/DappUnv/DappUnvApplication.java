8df366c38d7c0613120a77beb9fe91809dac2427
Talel Ben Belgacem <58850422+Talel12@users.noreply.github.com>
2023-11-25 21:09:22 +0100

    database config

--- DappUnv/src/main/java/tn/enig/DappUnv/DappUnvApplication.java
+++ DappUnv/src/main/java/tn/enig/DappUnv/DappUnvApplication.java
@@ -2,8 +2,13 @@ package tn.enig.DappUnv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ComponentScan ({"com.server", "com.server.config"})
+@EnableJdbcRepositories ("com.server.repository")
 public class DappUnvApplication {
 
 	public static void main(String[] args) {
