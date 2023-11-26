88a18ee3930455b93fcd853199dc3c189a0c9df2
Talel Ben Belgacem <58850422+Talel12@users.noreply.github.com>
2023-11-26 14:47:57 +0100

    @EnableMethodSecurity

--- DappUnv/src/main/java/tn/enig/DappUnv/DappUnvApplication.java
+++ DappUnv/src/main/java/tn/enig/DappUnv/DappUnvApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @ComponentScan ({"com.server", "com.server.config"})
 @EnableJdbcRepositories ("com.server.repository")
