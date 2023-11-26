d54ae2919fe6f73edfa0d329a2897482e2e153b8
Talel Ben Belgacem <58850422+Talel12@users.noreply.github.com>
2023-11-26 15:10:26 +0100

    annuler

--- DappUnv/src/main/java/tn/enig/DappUnv/DappUnvApplication.java
+++ DappUnv/src/main/java/tn/enig/DappUnv/DappUnvApplication.java
@@ -2,13 +2,13 @@ package tn.enig.DappUnv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 
-@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
 @ComponentScan ({"com.server", "com.server.config"})
 @EnableJdbcRepositories ("com.server.repository")
 public class DappUnvApplication {
